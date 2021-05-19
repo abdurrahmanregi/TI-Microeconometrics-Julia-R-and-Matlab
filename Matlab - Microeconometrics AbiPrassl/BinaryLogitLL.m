e87bc6add4fbc056f93d90d4680d2505612a2c35
Regi Kusumaatmadja <abdurrahman.regi@gmail.com>
2021-05-20 00:26:19 +0200

    Update II - ch04
    Update:
    - ch04
    - new files from companion website (not checked)

---
+++ Matlab - Microeconometrics AbiPrassl/BinaryLogitLL.m
@@ -0,0 +1,22 @@
+function [LL, ll_i] = BinaryLogitLL(Beta, y, x)
+% --------------------------------------------------
+% PURPOSE: Calculate the loglikelihood for the
+%          binary logit
+% --------------------------------------------------
+% INPUTS: Beta: Kx1 parameter vector
+%         y: Nx1 vector of dependent variable
+%         x: NxK matrix of independent variable
+% --------------------------------------------------
+% OUTPUT: LL: scalar log likelihood
+%         ll_i: log likelihood contribution of
+%               observations
+% --------------------------------------------------
+
+Lambda_xb   = exp(x * Beta)./(1 + exp(x * Beta));
+
+ll_i        = y .* log(Lambda_xb) ...
+    + (1 - y) .* log (1 - Lambda_xb);
+
+LL          = -sum(ll_i);
+
+return
